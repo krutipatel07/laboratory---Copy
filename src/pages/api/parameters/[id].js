8d56dbee1b86fbd33261f2e697eacfda88db300a
kirti18patel <kirti18patel@yahoo.com>
2021-12-17 16:19:35 -0500

    generate design model and it api's created

---
+++ src/pages/api/parameters/[id].js
@@ -0,0 +1,56 @@
+import dbConnect from "../../../utils/dbConnect";
+import { Parameter } from "../../../models"
+
+dbConnect();
+
+export default async (req, res) => {
+    const { 
+        query: {id},
+        method } = req;
+
+    switch (method) {
+        case 'GET':
+            try {
+                const parameter = await Parameter.findById(id);
+                if (!parameter){
+                    res.status(404).json({ success: false, message: error})
+                }
+                res.status(200).json({ success: true, data: parameter});
+                
+            } catch (error) {
+                res.status(404).json({ success: false, message: error})
+            }
+            break;
+        case 'PUT':
+            try {
+                const parameter = await Parameter.findByIdAndUpdate(id, req.body, {
+                    new: true,
+                    runValidators: true
+                });
+                if (!parameter){
+                    res.status(404).json({ success: false, message: error})
+                }
+                res.status(200).json({ success: true, data: parameter})
+                
+            } catch (error) {
+                res.status(404).json({ success: false, message: error})
+            }
+            break;
+        case 'DELETE':
+            try {
+                const deletedParameter = await Parameter.deleteOne({ _id: id });
+                if (!deletedParameter){
+                    res.status(404).json({ success: false, message: error})
+                }
+                
+                res.status(200).json({ success: true, data: deletedParameter})
+                
+            } catch (error) {
+                res.status(404).json({ success: false, message: error})
+            }
+            break;
+        default:
+            res.status(404).json({ success: false })
+            break;
+    }
+}
