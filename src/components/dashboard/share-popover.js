aa9523ff56b1dcc9a5fabafd08d946e05ed899aa
kruti <kruti.patel0709@gmail.com>
2021-12-28 13:38:06 -0500

    add share button popover

---
+++ src/components/dashboard/share-popover.js
@@ -0,0 +1,109 @@
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import toast from 'react-hot-toast';
+import {
+  Avatar,
+  Box,
+  Divider,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Popover,
+  Typography
+} from '@mui/material';
+import { Logout as LogoutIcon } from '@mui/icons-material';
+import { useAuth } from '../../hooks/use-auth';
+import { Cog as CogIcon } from '../../icons/cog';
+import { UserCircle as UserCircleIcon } from '../../icons/user-circle';
+import { SwitchHorizontalOutlined as SwitchHorizontalOutlinedIcon } from '../../icons/switch-horizontal-outlined';
+
+export const SharePopover = (props) => {
+  const { anchorEl, onClose, open, ...other } = props;
+  const router = useRouter();
+  const { logout } = useAuth();
+  // To get the user from the authContext, you can use
+  const { user } = useAuth();
+
+
+  return (
+    <Popover
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        horizontal: 'center',
+        vertical: 'bottom'
+      }}
+      keepMounted
+      onClose={onClose}
+      open={open}
+      PaperProps={{ sx: { width: 300 } }}
+      transitionDuration={0}
+      {...other}>
+      <Box
+        sx={{
+          alignItems: 'center',
+          p: 2,
+          display: 'flex'
+        }}
+      >
+        <Avatar
+          src={`https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=${user.email}`}
+          sx={{
+            height: 40,
+            width: 40
+          }}
+        >
+          <UserCircleIcon fontSize="small" />
+        </Avatar>
+        <Box
+          sx={{
+            ml: 1
+          }}
+        >
+          <Typography variant="body1">
+            {user.email}
+          </Typography>
+        </Box>
+      </Box>
+      <Divider />
+      <Box sx={{ my: 1 }}>
+        <NextLink
+          href="/dashboard/account"
+          passHref
+        >
+          <MenuItem component="a">
+            <ListItemIcon>
+              <CogIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText
+              primary={(
+                <Typography variant="body1">
+                  Settings
+                </Typography>
+              )}
+            />
+          </MenuItem>
+        </NextLink>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <LogoutIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText
+            primary={(
+              <Typography variant="body1">
+                Logout
+              </Typography>
+            )}
+          />
+        </MenuItem>
+      </Box>
+    </Popover>
+  );
+};
+
+SharePopover.propTypes = {
+  anchorEl: PropTypes.any,
+  onClose: PropTypes.func,
+  open: PropTypes.bool
+};
