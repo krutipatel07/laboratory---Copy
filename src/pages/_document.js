97cf502baa84c0dec5bfccd98eb2c8c9523cbe75
kirti18patel <kirti18patel@yahoo.com>
2022-01-18 13:59:01 -0500

    script tag fro 'clarity' added in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -50,6 +50,12 @@ class CustomDocument extends Document {
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}');`}}></script>
+
+          <script dangerouslySetInnerHTML={{ __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "a8r4fbga2f");`}}></script>
         </Head>
         <body>
           <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}"
