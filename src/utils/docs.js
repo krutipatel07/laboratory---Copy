5691333ba779b45e87fcf511d53f06e0800e1c91
kruti <kruti.patel0709@gmail.com>
2022-01-20 15:33:35 -0500

    remove slug error

--- src/utils/docs.js
+++ src/utils/docs.js
@@ -1,40 +1,40 @@
-// import { readdirSync, readFileSync } from 'fs';
-// import { join } from 'path';
-// import matter from 'gray-matter';
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+import matter from 'gray-matter';
 
-// // const articlesDirectory = join(process.cwd(), '_docs');
+const articlesDirectory = join(process.cwd(), '_docs');
 
-// export const getArticleSlugs = () => {
-//   // return readdirSync(articlesDirectory);
-// };
+export const getArticleSlugs = () => {
+  return readdirSync(articlesDirectory);
+};
 
-// export const getArticleBySlug = (slug, fields = []) => {
-//   const realSlug = slug.replace(/\.md$/, '');
-//   // const fullPath = join(articlesDirectory, `${realSlug}.md`);
-//   const fileContents = readFileSync(fullPath, 'utf8');
-//   const { data, content } = matter(fileContents);
+export const getArticleBySlug = (slug, fields = []) => {
+  const realSlug = slug.replace(/\.md$/, '');
+  const fullPath = join(articlesDirectory, `${realSlug}.md`);
+  const fileContents = readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
 
-//   const items = {};
+  const items = {};
 
-//   fields.forEach((field) => {
-//     if (field === 'slug') {
-//       items[field] = realSlug;
-//     }
+  fields.forEach((field) => {
+    if (field === 'slug') {
+      items[field] = realSlug;
+    }
 
-//     if (field === 'content') {
-//       items[field] = content;
-//     }
+    if (field === 'content') {
+      items[field] = content;
+    }
 
-//     if (data[field]) {
-//       items[field] = data[field];
-//     }
-//   });
+    if (data[field]) {
+      items[field] = data[field];
+    }
+  });
 
-//   return items;
-// };
+  return items;
+};
 
-// export const getArticles = (fields = []) => {
-//   const slugs = getArticleSlugs();
+export const getArticles = (fields = []) => {
+  const slugs = getArticleSlugs();
 
-//   return slugs.map((slug) => getArticleBySlug(slug, fields));
-// };
+  return slugs.map((slug) => getArticleBySlug(slug, fields));
+};
