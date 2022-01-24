2626e28f9941e1e0a7a647189ac42f775f5a9d71
kirti18patel <kirti18patel@yahoo.com>
2022-01-24 13:56:32 -0500

    scripts updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -55,7 +55,7 @@ class CustomDocument extends Document {
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src=“https://www.clarity.ms/tag/”+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, “clarity”, “script”, “abuzzbuw7h”);`}}></script>
+            })(window, document, 'clarity', 'script', 'abuzzbuw7h');`}}></script>
 
           <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
