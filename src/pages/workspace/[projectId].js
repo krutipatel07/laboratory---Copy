49f96a530c5e4c9ea9ccd3d50310d035495b9dfc
kruti <kruti.patel0709@gmail.com>
2022-03-01 12:19:51 -0500

    whiteboard full width

--- src/pages/workspace/[projectId].js
+++ src/pages/workspace/[projectId].js
@@ -160,10 +160,10 @@ return (
           <Grid item 
           xs={12}>
             <Container maxWidth="xl"> 
-              <Box
+              <Box fullWidth
               sx={{
-                maxWidth: 1260,
-                width: '100%',
+                // maxWidth: 1260,
+                maxWidth: '100%',
                 mx: 'auto',
                 height: '600px'
               }}>
