48dcf15658938d082c2762428ebb262ea1dc0b6c
kirti18patel <kirti18patel@yahoo.com>
2022-03-08 09:02:11 -0500

    email invite link

--- src/pages/api/emails/invite.js
+++ src/pages/api/emails/invite.js
@@ -8,7 +8,7 @@ export default async (req, res) => {
 
     Join our collaborative workspace using the link below.\r\n\r\n
 
-    https://laboratory-fpqdfw3s0-maket-development.vercel.app/workspace/collaborator?invite=true&projectId=${body.projectId}&designId=${body.designId}
+    https://laboratory-maket-development.vercel.app/workspace/collaborator?invite=true&projectId=${body.projectId}&designId=${body.designId}
     `;
     const data = { 
         to: body.email,
