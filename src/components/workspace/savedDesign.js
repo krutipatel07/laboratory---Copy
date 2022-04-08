21f190f99a3c67f68f4c4c2991529450c8b5742f
kirti18patel <kirti18patel@yahoo.com>
2022-04-08 11:59:55 -0400

    pdf allowed in import design

--- src/components/workspace/savedDesign.js
+++ src/components/workspace/savedDesign.js
@@ -196,7 +196,7 @@ const importDesign = async (secure_url, designName) => {
               variant="standard"
             />
             <FileDropzone
-                accept="image/*"
+                accept="image/*,.pdf"
                 files={files}
                 onDrop={handleDrop}
                 onRemove={handleRemove}
