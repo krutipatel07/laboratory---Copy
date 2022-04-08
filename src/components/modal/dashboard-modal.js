12c5b50b5ba9f3caab9a2ff0214104ae648e5df2
kruti <kruti.patel0709@gmail.com>
2022-04-08 15:58:06 -0400

    update onboarding images and make responsive

--- src/components/modal/dashboard-modal.js
+++ src/components/modal/dashboard-modal.js
@@ -43,44 +43,44 @@ export default function DashboardModal(props) {
   const setModalShown = props.setModalShown;
   
   const welcomeGuide = [{
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078808/maket_design/Screen_Shot_2022-03-17_at_10.04_2_hs1y2m.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/Tutorial_sidebar.png',
     title: 'Welcome to your Project Dashboard!',
     description: 'This quick tutorial will show you how to navigate the platform and generate your first designs. If you would like to skip and explore on your own, you can always revisit this tutorial in your side navigation bar'
   },{
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078053/maket_design/Screen_Shot_2022-03-17_at_10.13_1_zgu72s.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/Create_new_project.png',
     title: 'Create a new project',
     description: 'To begin, click on the create new project button in the top right corner of the screen'
   },{
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078053/maket_design/Screen_Shot_2022-03-17_at_10.20_1_opsydv.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/Generate.png',
     title: 'Generate a design',
     description: 'To generate your first design, input your design constraints using the dropdown menu When you’ve inputted all your constraints, click on the generate button'
   },{
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078052/maket_design/Screen_Shot_2022-03-17_at_10.25_1_e6ootj.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/project_card.png',
     title: 'Save designs to your project',
     description: 'Now that you’ve generated your floorplans using your specified constraints, you can browse the floorplans and save your favorite floorplans to your project. '
   },{
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078052/maket_design/Group_91_fg5dxg.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/Generate_tab.png',
     title: 'Save designs to your project',
     description: 'Once you have saved your favorite designs, you can view and edit them on the designs page'
   },
 
   {
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078052/maket_design/Group_91_fg5dxg.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/import.png',
     title: 'Edit and collaborate on your design',
     description: 'Now that you are in your saved designs page, you can edit and collaborate on the floorplans. Click on the floorplan that you want to edit.'
   },
   {
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078052/maket_design/Group_91_fg5dxg.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/Collaborators.png',
     title: 'Add collaborators',
     description: 'You are now in the editing page. To add a collaborator to your design, click on the button on the top right corner of the page'
   },
   {
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078052/maket_design/Group_91_fg5dxg.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/limnu_bar.png',
     title: 'Edit your design',
     description: 'Use the bottom tool bar to edit your design. You can add comments for your collaborators, you can draw on the plan, and add arrows and symbols.Once you have finished editing, you can also export your design.'
   },
   {
-    img: 'https://res.cloudinary.com/maket/image/upload/v1649078052/maket_design/Group_91_fg5dxg.png',
+    img: 'https://maket-laboratory-content.s3.ca-central-1.amazonaws.com/onboarding/Version.png',
     title: 'Save new versions',
     description: 'After you have edited your design, you can create new versions of the design and start fresh by pressing on the button a the bottom left of the screen.'
   }]
@@ -116,9 +116,10 @@ export default function DashboardModal(props) {
         </DialogContentText>
       </DialogTitle>
 
-      <DialogContent>
+      <DialogContent sx={{pt:50}}>
         <Card sx={{
           margin: "auto",
+          
           backgroundColor: 'background.paper',
           '&:hover': {
             backgroundColor: 'background.hover',
@@ -127,7 +128,8 @@ export default function DashboardModal(props) {
           <CardMedia
             component="img"
             alt="green iguana"
-            height="260"
+            // height="260"
+            height='100%'
             src={welcomeGuide[index].img}
           />
         </Card>
