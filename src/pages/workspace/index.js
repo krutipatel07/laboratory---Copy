a1daf363751e36ccce9ee18dc6e6243555109c89
kirti18patel <kirti18patel@yahoo.com>
2022-04-19 09:22:57 -0400

    generate import modal issue resolved

--- src/pages/workspace/index.js
+++ src/pages/workspace/index.js
@@ -58,8 +58,8 @@ function a11yProps(index) {
 
 const ProjectWorkspace = withRouter((props) => {
   const router = useRouter();
-  const i = router.query.i;
-  const [value, setValue] = React.useState(1);
+  const index = parseInt(router.query.i);
+  const [value, setValue] = React.useState(index || 1);
   const [newDesign, setNewDesign] = React.useState(0);
 
   const handleChange = (event, newValue) => {
