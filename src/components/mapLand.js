2a5277385dc8accd4b9d3e9b5c3d475ce5bc2404
kirti18patel <kirti18patel@yahoo.com>
2022-06-01 12:29:02 -0400

    search functionality added

--- src/components/mapLand.js
+++ src/components/mapLand.js
@@ -11,7 +11,13 @@ import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import { Box } from '@mui/material';
 import {EditControl} from "react-leaflet-draw"
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 
 const Map = (props) => {
   const mapUpdate = props.mapUpdate;
@@ -22,10 +28,11 @@ const Map = (props) => {
   const [saved, setSaved] = useState(true)
   const blueOptions = { color: 'blue' }
   const [zoom, setZoom] = useState(11)
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState([]);
   
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem('layers'))
+    const center = JSON.parse(localStorage.getItem('center'))
       if (data){
         data.zoom && setZoom(data.zoom)
         let polygon=[];
@@ -33,11 +40,11 @@ const Map = (props) => {
           polygon = [...polygon, [lat_lngs.lat, lat_lngs.lng]]
         );
         setMapLayers(layers => [...layers, [polygon]])
-        setCenter({lat : polygon[0][0], lng : polygon[0][1]});
+       center ? setCenter(center) : setCenter({lat : polygon[0][0], lng : polygon[0][1]});
       }
       else {
         setMapLayers([])
-        setCenter({lat: 45.53, lng:  -73.62})
+        center ? setCenter(center) : setCenter({lat: 45.53, lng:  -73.62})
       }
   },[mapUpdate])
   
@@ -83,29 +90,35 @@ const Map = (props) => {
       setSaved(true)
     }
 
-    const getCoordinates = (address) => {
-      fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+address+'&key='+API_KEY)
+    const getCoordinates = (e) => {
+      e.preventDefault();
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery}.json?access_token=pk.eyJ1IjoibWFrZXQiLCJhIjoiY2wycTZ5bmVtMDNlbzNubnM2YW5rM3J0aSJ9.BJyV0xNP08sXipMK5SX-HQ`)
         .then(response => response.json())
         .then(data => {
-          const latitude = data.results.geometry.location.lat;
-          const longitude = data.results.geometry.location.lng;
-          console.log({latitude, longitude})
+          console.log(data);
         })
     }
-    console.log(mapRef)
 
   return (
     <>
     <Box sx={{mb:2}}>
       <form 
-        onChange={() => console.log("onChange")}
-        onRequestSearch={() => console.log("onRequestSearch")}
       >
         <TextField
           id="search-bar"
           className="text"
-          onInput={(e) => {
-            setSearchQuery(e.target.value);
+          onChange={(e) => {
+            setSearchQuery([])
+            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${e.target.value}.json?access_token=pk.eyJ1IjoibWFrZXQiLCJhIjoiY2wycTZ5bmVtMDNlbzNubnM2YW5rM3J0aSJ9.BJyV0xNP08sXipMK5SX-HQ`)
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              data.features.forEach(data => setSearchQuery(search => [...search, {
+                place_name : data.place_name,
+                center : data.center
+              }])
+              );
+            })
           }}
           label="Enter a city name"
           variant="outlined"
@@ -117,6 +130,37 @@ const Map = (props) => {
         </IconButton>
       </form>
     </Box>
+
+    {searchQuery.length ? 
+      <List>
+        { searchQuery.map((search,i) => (
+            <ListItem
+              onClick={ () =>{ 
+                localStorage.setItem('center', JSON.stringify({lat : search.center[1], lng : search.center[0]}))
+                setMapUpdate((prev) => !prev)}
+              }
+              key={i}
+              sx={{
+                cursor: "pointer",
+                '& + &': {
+                  mt: 1
+                }
+              }}
+            >
+              <ListItemIcon>
+                <LocationOnIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText
+                primary={search.place_name}
+                primaryTypographyProps={{
+                  color: 'textPrimary',
+                  variant: 'subtitle2'
+                }}
+              />
+            </ListItem>
+        ))}
+      </List>: null
+    }
 
     {center && <MapContainer center={center} zoom={zoom} scrollWheelZoom={false} ref={mapRef} style={{ height: "70vh", width: "100%"  }}>
       <TileLayer
