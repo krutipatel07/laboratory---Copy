d61db98fd583f24d0fdb48295ed035657532fe2f
Kruti Patel <69260250+krutipatel07@users.noreply.github.com>
2022-10-21 14:54:47 -0400

    redesign button+logo show and hide logic  (#26)

--- src/components/workspace/generateDesignTab.js
+++ src/components/workspace/generateDesignTab.js
@@ -1,20 +1,35 @@
-import React, { useState, } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Box, Button, Container, Typography} from '@mui/material';
 import { withAuthGuard } from '../../hocs/with-auth-guard';
 import { withRouter, useRouter } from 'next/router'
 import { Logo } from '../logo';
 import ConstraintsFloor01 from './constraintsFloor01';
+import axios from 'axios'
+
 
 const GenerateDesignTab = withRouter((props) => {
   const [click, setClick] = useState(false)
-  const [show, setShow] = useState(true)
   const router = useRouter();
+  const [length,setLength] = useState()
+  const projectId = router.query.id || router.query.projectId;
+  const [show, setShow] = useState(true)
 
 
   const handleClick = async (e) => {
-    setClick(!click)
     setShow(false)
   };
+
+  useEffect(() => {
+    axios.get(`/api/projects/${projectId}`)
+      .then(res => {
+          // fetch existing parameters form a project
+          const length = res.data.data.search_parameters.length;
+          setLength(length);
+        }
+      )
+      .catch(error => console.log(error));
+      setShow(true)
+  }, [projectId]);
 
   return (
     <>
@@ -25,7 +40,7 @@ const GenerateDesignTab = withRouter((props) => {
           mb: 8, mt: 4
         }}
       >
-        {show &&
+       {show && length===0 ?
         <Container maxWidth="xl" sx={{textAlign: "center"}}>
             <Typography sx={{textAlign: 'center', fontSize: '20px', paddingTop: '100px'}}>Let's generate your first design</Typography>
             <Logo
@@ -39,8 +54,8 @@ const GenerateDesignTab = withRouter((props) => {
                 />
             <div><Button variant="contained" onClick={handleClick} sx={{mt: 3, '&:hover': {backgroundColor: "#000000"}}}>
                 GENERATE DESIGNS</Button></div>
-        </Container>}
-        {click ?<ConstraintsFloor01 setValue={props.setValue} projectId= {props.router.query.id}/> : null }
+        </Container> : <ConstraintsFloor01 setValue={props.setValue} projectId= {props.router.query.id}/> }   
+
       </Box>
     </>
   );
